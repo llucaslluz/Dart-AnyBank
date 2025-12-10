39543dd2b22bbc2d087c78a54c4a1ee6b149f1fd
Lucas da Luz Morais <llucaslluz@gmail.com>
2025-12-09 22:51:50 -0300

    Criando contas

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,4 +1,16 @@
 
 void main() {
+  List<String> contas = <String> ["Mateus","Roberta"];
+  List<double> saldo = <double> [1000, 1000];
+
+  Conta contaMateus = Conta("Mateus", 1000);
+  Conta contaRoberta = Conta("Roberta", 2000);
+}
+
+class Conta {
+  String titular;
+  double saldo;
+
+  Conta(this.titular, this.saldo);
 
 }
