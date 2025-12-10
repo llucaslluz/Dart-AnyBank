27f9232f076a1888093a16ad44efdb84174ca151
Lucas da Luz Morais <llucaslluz@gmail.com>
2025-12-09 22:59:35 -0300

    modificando saldos

--- bin/anybank.dart
+++ bin/anybank.dart
@@ -1,10 +1,27 @@
 
 void main() {
-  List<String> contas = <String> ["Mateus","Roberta"];
-  List<double> saldo = <double> [1000, 1000];
+//  List<String> contas = <String> ["Mateus","Roberta"];
+//  List<double> saldo = <double> [1000, 1000];
 
   Conta contaMateus = Conta("Mateus", 1000);
   Conta contaRoberta = Conta("Roberta", 2000);
+
+  List<Conta> contas = <Conta> [contaMateus, contaRoberta];
+
+  // print (contaMateus.titular);
+  // print(contaMateus.saldo);
+
+  // print(contaRoberta.titular);
+  // print(contaRoberta.saldo);
+
+  for (Conta conta in contas) {
+    print(conta.titular);
+    print(conta.saldo);
+  }
+
+  contaRoberta.saldo =5000;
+
+    print(contaRoberta.saldo);
 }
 
 class Conta {
